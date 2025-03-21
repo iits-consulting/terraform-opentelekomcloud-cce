3f563cde1a97ade22bbbe9c8662b23c63051a6f6
Zeljko Bekcic <zeljko.bekcic@iits-consulting.de>
2025-03-21 09:55:46 +0100

    chore: set versions and add ci

--- versions.tf
+++ versions.tf
@@ -1,11 +1,23 @@
 terraform {
+
+  required_version = ">= 1.4.0"
+
   required_providers {
     opentelekomcloud = {
       source  = "opentelekomcloud/opentelekomcloud"
       version = ">=1.32.0"
     }
     errorcheck = {
-      source = "iits-consulting/errorcheck"
+      source  = "iits-consulting/errorcheck"
+      version = "3.0.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.0.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">=4.0.0"
     }
   }
 }
