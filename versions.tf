638f40c488175e16a2807afc0ff65b2affa56304
Zeljko Bekcic <zeljko.bekcic@iits-consulting.de>
2025-03-24 16:28:58 +0100

    update versions

--- versions.tf
+++ versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     opentelekomcloud = {
       source  = "opentelekomcloud/opentelekomcloud"
-      version = ">=1.32.0"
+      version = "~> 1.32"
     }
     errorcheck = {
       source  = "iits-consulting/errorcheck"
@@ -13,11 +13,11 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = ">=3.0.0"
+      version = "~> 3.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = ">=4.0.0"
+      version = "~> 4.0"
     }
   }
 }
